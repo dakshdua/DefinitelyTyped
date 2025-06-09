8fcf18f22012a495a7f74d8cd09db870577073cb
Daksh Dua <duadaksh@gmail.com>
2025-06-09 11:27:12 -0700

    Update d3.mode to accept non-numeric types

--- types/d3-array/index.d.ts
+++ types/d3-array/index.d.ts
@@ -157,7 +157,7 @@ export function extent<T, U extends Numeric>(
  * An optional accessor function may be specified, which is equivalent to calling Array.from before computing the mode.
  * This method ignores undefined, null and NaN values; this is useful for ignoring missing data.
  */
-export function mode(iterable: Iterable<Numeric | undefined | null>): number;
+export function mode<T extends Primitive | Numeric>(iterable: Iterable<T | undefined | null>): T;
 /**
  * Returns the mode of the given iterable, i.e. the value which appears the most often.
  * In case of equality, returns the first of the relevant values.
@@ -165,9 +165,9 @@ export function mode(iterable: Iterable<Numeric | undefined | null>): number;
  * An optional accessor function may be specified, which is equivalent to calling Array.from before computing the mode.
  * This method ignores undefined, null and NaN values; this is useful for ignoring missing data.
  */
-export function mode<T>(
+export function mode<T extends Primitive | Numeric>(
     iterable: Iterable<T>,
-    accessor: (datum: T, index: number, array: Iterable<T>) => number | undefined | null,
+    accessor: (datum: T, index: number, array: Iterable<T>) => Primitive | undefined | null,
 ): number;
 
 /**
